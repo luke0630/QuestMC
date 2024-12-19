d3112a4009a9a50ce96a5542e1e9c29f401a9a3c
luke0630 <ryuto-0915@outlook.jp>
2024-12-20 02:25:27 +0900

    add: GUITypes

---
+++ src/main/java/org/luke/questMC/GUI/GUITypes.java
@@ -0,0 +1,11 @@
+package org.luke.questMC.GUI;
+
+public class GUITypes {
+    public enum ListGUIEnum {
+        Quests,
+        Quest_Detail,
+    }
+    public enum GUIEnum {
+        Home
+    }
+}
