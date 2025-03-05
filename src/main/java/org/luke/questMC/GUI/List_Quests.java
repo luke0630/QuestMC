b2de3f8eac0145de3103f5d2e42d5ea1b575f6e5
luke0630 <ryuto-0915@outlook.jp>
2025-03-05 21:26:24 +0900

    import

--- src/main/java/org/luke/questMC/GUI/List_Quests.java
+++ src/main/java/org/luke/questMC/GUI/List_Quests.java
@@ -1,6 +1,5 @@
 package org.luke.questMC.GUI;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
